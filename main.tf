5edaf7b4da833cc66b6e620c87c944ed632df3cd
Tony Owens <anthony.owens1987@gmail.com>
2023-02-22 23:46:00 -0800

    Initial commit

---
+++ main.tf
@@ -0,0 +1,66 @@
+resource "azurerm_kubernetes_cluster" "aks" {
+  count               = var.resource_count > 0 ? 1 : 0
+  name                = "${var.name}-${var.environment}-aks"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  dns_prefix                   = var.dns_prefix
+  image_cleaner_enabled        = true
+  image_cleaner_interval_hours = 72
+  kubernetes_version           = var.kubernetes_version
+  oidc_issuer_enabled          = true
+  sku_tier                     = var.environment == "dev" ? "Free" : "Paid"
+  tags                         = var.tags
+
+  azure_active_directory_role_based_access_control {
+    managed                = true
+    admin_group_object_ids = var.admin_group_object_ids
+  }
+
+  default_node_pool {
+    enable_auto_scaling = var.enable_auto_scaling
+    max_count           = var.enable_auto_scaling ? var.max_node_count : null
+    max_pods            = var.max_pods
+    min_count           = var.enable_auto_scaling ? var.min_node_count : null
+    name                = var.name
+    node_count          = var.node_count
+    vm_size             = var.vm_size
+  }
+
+  network_profile {
+    network_plugin = "none"
+    pod_cidr       = "192.168.0.0/16"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      default_node_pool[0].node_count
+    ]
+  }
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "node_pool" {
+  for_each = { for k, v in var.additional_node_pools : k => v if var.resource_count > 0 }
+  name     = each.key
+
+  enable_auto_scaling   = true
+  kubernetes_cluster_id = try(azurerm_kubernetes_cluster.aks[0].id, null)
+  max_count             = try(each.value.max_node_count, null)
+  max_pods              = 60
+  min_count             = try(each.value.min_node_count, null)
+  node_count            = each.value.node_count
+  node_taints           = try(each.value.node_taints, null)
+  orchestrator_version  = each.value.orchestrator_version
+  tags                  = try(each.value.tags, null)
+  vm_size               = each.value.vm_size
+
+  lifecycle {
+    ignore_changes = [
+      node_count
+    ]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,92 @@
+variable "additional_node_pools" {
+  type = map(any)
+  default = {
+    example = {
+      max_node_count       = 4
+      min_node_count       = 2
+      node_count           = 2
+      orchestrator_version = "1.25.0"
+      vm_size              = "Standard_B1ms"
+    }
+  }
+  description = "A map that describes the configuration for additional node pools."
+}
+
+variable "admin_group_object_ids" {
+  type        = list(string)
+  default     = []
+  description = "A list of Azure AD `user`/`group`/`service principals` that will be provided `ClusterAdmin` rights."
+}
+
+variable "resource_count" {
+  type        = number
+  default     = 0
+  description = "The number of AKS clusters to provision."
+}
+
+variable "dns_prefix" {
+  type        = string
+  description = "The DNS prefix for the AKS cluster."
+}
+
+variable "enable_auto_scaling" {
+  type        = bool
+  default     = true
+  description = "A flag to enable node auto scaling."
+}
+
+variable "environment" {
+  type        = string
+  description = "The name of the deployment environment for the resource."
+}
+
+variable "location" {
+  type        = string
+  description = "The Azure region where the resource will be provisioned."
+}
+
+variable "kubernetes_version" {
+  type        = string
+  default     = "1.25.0"
+  description = "The version of Kubernetes."
+}
+
+variable "name" {
+  type        = string
+  description = "The name to provide to the resource."
+}
+
+variable "node_count" {
+  type        = number
+  default     = 2
+  description = "The number of nodes to initially provision."
+}
+
+variable "max_node_count" {
+  type        = number
+  default     = 4
+  description = "The maximum number of nodes availalbe in the auto scaler pool."
+}
+
+variable "max_pods" {
+  type        = number
+  default     = 30
+  description = "The maximum number of pods available to deploy on a node."
+}
+
+variable "min_node_count" {
+  type        = number
+  default     = 1
+  description = "The minimum number of nodes available in the auto scaler pool."
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "The Azure resource group where the resources will be provisioned."
+}
+
+variable "vm_size" {
+  type        = string
+  default     = "Standard_B1ms"
+  description = "The VM sku to use for the `default` node pool"
+}
