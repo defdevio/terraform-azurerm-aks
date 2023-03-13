6a9a29f1a83fb21ce5b964544bc5c63540765bbb
Tony Owens <anthony.owens1987@gmail.com>
2023-03-13 16:47:17 -0700

    Added helpers to reduce repeat code

--- aks_test.go
+++ aks_test.go
@@ -3,13 +3,12 @@ package tests
 import (
 	"context"
 	"fmt"
-	"log"
 	"os"
 	"path"
 	"strings"
 	"testing"
 
-	"github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2020-10-01/resources"
+	"github.com/defdevio/terratest-helpers/pkg/helpers"
 	"github.com/gruntwork-io/terratest/modules/azure"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
@@ -48,45 +47,6 @@ func terraformVars() map[string]any {
 	return testVars
 }
 
-// Creates a file if does not already exist on the specified path
-func CreateFile(path string, content string) error {
-	if _, err := os.Stat(path); err != nil {
-		if os.IsNotExist(err) {
-			bytes := []byte(content)
-			err = os.WriteFile(path, bytes, 0644)
-			if err != nil {
-				return err
-			}
-		}
-	}
-
-	return nil
-}
-
-// Creates the required provider file on the system
-func CreateProviderFile(providerFilePath string, t *testing.T) error {
-	providerContent := `
-provider "azurerm" {
-	features {}
-}`
-
-	err := CreateFile(providerFilePath, providerContent)
-	return err
-}
-
-// Cleans up the common files created by terraform during init, plan, and apply
-func CleanUpTestFiles(t *testing.T, files []string) error {
-	for _, file := range files {
-		filePath := path.Join(workDir, file)
-		err := os.RemoveAll(filePath)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
 func TestCreateAKSClusterWithNodePool(t *testing.T) {
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: "",
@@ -104,10 +64,10 @@ func TestCreateAKSClusterWithNodePool(t *testing.T) {
 	}
 
 	// Defer cleaning up the test files created during the test
-	defer CleanUpTestFiles(t, testFiles)
+	defer helpers.CleanUpTestFiles(t, testFiles, workDir)
 
 	// Create the provider file
-	err := CreateProviderFile(path.Join(workDir, "provider.tf"), t)
+	err := helpers.CreateAzureProviderFile(path.Join(workDir, "provider.tf"), t)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -141,32 +101,15 @@ func TestCreateAKSClusterWithNodePool(t *testing.T) {
 
 	// This will make a new slice array that contains the keys of the
 	// additional_node_pools map
-	nodeKeys := make([]string, len(additionalNodePools))
-	i := 0
-	for key := range additionalNodePools {
-		nodeKeys[i] = key
-		i++
-	}
-
-	// Create a resource group client
-	resourceGroupClient, err := azure.CreateResourceGroupClientE(subscriptionID)
-	if err != nil {
-		t.Fatal(err)
-	}
+	nodeKeys := helpers.GetMapKeys(t, additionalNodePools)
 
 	// Defer the deletion of the resource group until all test functions have finished
-	defer resourceGroupClient.Delete(ctx, resourceGroup)
+	defer helpers.DeleteAzureResourceGroup(t, subscriptionID, resourceGroup)
 
-	// Create the resource group using the resourceGroupClient
-	resp, err := resourceGroupClient.CreateOrUpdate(ctx, resourceGroup, resources.Group{
-		Location: &location,
-	})
+	// Create the resource group
+	err = helpers.CreateAzureResourceGroup(t, subscriptionID, resourceGroup, &location)
 	if err != nil {
 		t.Fatal(err)
-	}
-
-	if resp.StatusCode == 201 {
-		log.Printf("Created resource group '%s'", *resp.Name)
 	}
 
 	// Defer destroying the terraform resources until the rest of the test functions finish
